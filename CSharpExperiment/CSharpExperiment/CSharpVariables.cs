916f9e71c3323279e0ad47ef4a12409c0bcca549
wheejuni <wheejuni@gmail.com>
2017-12-28 23:25:17 +0900

    [Step1] for문 사용법 체험, 전역변수 - 지역변수 구분 학습

---
+++ CSharpExperiment/CSharpExperiment/CSharpVariables.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExperiment
+{
+    class CSharpVariables
+    {
+        int globalVariable;
+        const int MAX = 1024;
+
+        public void forLoop()
+        {
+            for(int i = 0; i < MAX; i++)
+            {
+                Console.WriteLine(i);
+            }
+            globalVariable = 100;
+            Console.WriteLine("전역 변수를 할당하였다!");
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                CSharpVariables variables = new CSharpVariables();
+                Console.WriteLine(CSharpVariables.MAX);
+                variables.forLoop();
+            }
+        }
+    }
+}
