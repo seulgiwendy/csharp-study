08e177ab73f95072f7aaa1e3b4d2a9a4c45e5770
wheejuni <wheejuni@gmail.com>
2017-12-28 23:00:06 +0900

    [project] 기초문법 학습용 프로젝트 생성

---
+++ CSharpExperiment/CSharpExperiment/HelloCsharp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpExperiment
+{
+    class HelloCsharp
+    {
+        static void Main(string [] args)
+        {
+            Console.WriteLine("fuck you world!");
+
+            Console.WriteLine("press 'any key' to exit.");
+            Console.ReadKey();
+        }
+    }
+}
