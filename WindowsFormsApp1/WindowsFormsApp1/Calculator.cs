b6424a7e4d757e0e77b2249e1d7c976f54d573b0
wheejuni <wheejuni@gmail.com>
2017-12-31 23:13:41 +0900

    [project] started new Windows Forms project

---
+++ WindowsFormsApp1/WindowsFormsApp1/Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        private void HelloLabel_Click(object sender, EventArgs e)
+        {
+            HelloLabel.Text = "클릭했구먼. 말을 잘 듣는구먼.";
+        }
+    }
+}
